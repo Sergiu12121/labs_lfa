2bd694a7c043eea5225a275a329c3e1980dcc2b6
Sergiu12121 <sergiu.dimbitchi@mail.ru>
2024-02-14 14:29:13 +0200

    The final code (hopefully)

--- lab_1/lab1_v6.py
+++ lab_1/lab1_v6.py
@@ -1,21 +1,50 @@
-class FiniteAutomaton:
+import random
 
+class Grammar:
+    def __init__(self, variables, terminals, productions, start_variable):
+        self.variables = set(variables)
+        self.terminals = set(terminals)
+        self.productions = productions
+        self.start_variable = start_variable
+
+    def generate_string(self):
+        def generate_from(variable):
+            production = random.choice(self.productions[variable])
+            return ''.join(generate_from(sym) if sym in self.variables else sym for sym in production)
+        
+        return generate_from(self.start_variable)
+
+    def generate_strings(self, n):
+        return [self.generate_string() for _ in range(n)]
+
+    def to_finite_automaton(self):
+
+        states = self.variables.union({'F'})
+        alphabet = self.terminals
+        transition_function = {}
+        accept_states = set()
+
+        for variable, expansions in self.productions.items():
+            for expansion in expansions:
+                if expansion != 'e':  
+                    if len(expansion) == 2 and expansion[1] in self.variables:
+                        transition_function[(variable, expansion[0])] = expansion[1]
+                    else:
+                        transition_function[(variable, expansion[0])] = 'F'
+                        accept_states.add('F')
+                else:
+                    accept_states.add(variable)
+
+        return FiniteAutomaton(states, alphabet, transition_function, self.start_variable, accept_states)
+
+
+class FiniteAutomaton:
     def __init__(self, states, alphabet, transition_function, start_state, accept_states):
         self.states = states
         self.alphabet = alphabet
         self.transition_function = transition_function
         self.start_state = start_state
         self.accept_states = accept_states
-
-    def __str__(self):
-        fa_str = "States: {}\n".format(self.states)
-        fa_str += "Alphabet: {}\n".format(self.alphabet)
-        fa_str += "Start State: {}\n".format(self.start_state)
-        fa_str += "Accept States: {}\n".format(self.accept_states)
-        fa_str += "Transition Function:\n"
-        for (state, symbol), next_state in self.transition_function.items():
-            fa_str += "  δ({}, {}) = {}\n".format(state, symbol, next_state)
-        return fa_str
 
     def check_string(self, input_string):
         current_state = self.start_state
@@ -29,24 +58,32 @@ class FiniteAutomaton:
         return current_state in self.accept_states
 
 
-finite_automaton = FiniteAutomaton(
-    states={'S', 'I', 'J', 'K', 'F'},
-    alphabet={'a', 'b', 'c', 'e', 'n', 'f', 'm'},
-    transition_function={
-        ('S', 'c'): 'I',
-        ('I', 'b'): 'J',
-        ('I', 'f'): 'I',
-        ('J', 'n'): 'J',
-        ('J', 'c'): 'S',
-        ('J', 'e'): 'K',
-        ('K', 'n'): 'K',
-        ('K', 'e'): 'F',
-        ('K', 'm'): 'F'
-    },
-    start_state='S',
-    accept_states={'K', 'F'}
-)
+# a. Creating the grammar object
+variables = ['S', 'I', 'J', 'K']
+terminals = ['a', 'b', 'c', 'e', 'n', 'f', 'm']
+productions = {
+    'S': ['cI'],
+    'I': ['bJ', 'fI'],
+    'J': ['nJ', 'cS', 'eK'],
+    'K': ['nK', 'e', 'm']
+}
+start_variable = 'S'
+grammar = Grammar(variables, terminals, productions, start_variable)
 
-test_strings = ['cnb', 'fm', 'ccce', 'cbne']
-results = {s: finite_automaton.check_string(s) for s in test_strings}
-print(results)
+# b. Generating 5 valid strings from the grammar
+valid_strings = grammar.generate_strings(5)
+
+# c. Converting the grammar to a finite automaton
+finite_automaton = grammar.to_finite_automaton()
+
+# d. Checking if certain strings can be obtained from the finite automaton
+print()
+test_strings = input("Input the word to check ")
+
+print ("5 VALID STRINGS :")
+for i in valid_strings:
+    print(i)
+print()
+print(f"The word {test_strings} is {finite_automaton.check_string(test_strings)}")
+
+
